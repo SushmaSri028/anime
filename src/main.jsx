8d4cdb3463d7879cc7050794a3cef01a7708adc1
Sushma Sri Kondamareddy <sushmasrikondamareddy430@gmail.com>
2026-01-07 12:53:58 +0530

    Add comments for React Router integration
    Added comments for potential React Router usage.

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+// If you are using React Router, import it here:
+// import { BrowserRouter } from 'react-router-dom' 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    {/* If using Router, wrap <App /> with <BrowserRouter basename="/anime"> */}
     <App />
   </StrictMode>,
 )
